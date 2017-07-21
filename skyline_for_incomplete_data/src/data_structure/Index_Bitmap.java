216f2b8b4e416fd1fd298ae3e738bd5ab560c0e9
Xlee9403 <Xlee9403@163.com>
2017-07-21 21:09:48 +0800

    Iskyline divide

---
+++ skyline_for_incomplete_data/src/data_structure/Index_Bitmap.java
@@ -0,0 +1,13 @@
+package src.data_structure;
+
+/**
+ * Created by Xue on 2017/7/21.
+ */
+public class Index_Bitmap
+{
+    public int block_num;
+    public int block_size;
+    public byte bitmap;
+
+
+}
